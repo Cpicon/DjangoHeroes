2ff05356b8d4448cfbeea92918934071f5d0d535
Cpicon <c.picon@uniandes.edu.co>
2021-01-04 16:40:56 -0500

    4. How to enable filtering on calculated fields? cap

--- DjangoHeroes/admin.py
+++ DjangoHeroes/admin.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
 
-#models
+# models
 from .models import *
 
-#utils
+# utils
 from django.db.models import Count
+
 # Register your models here.
-admin.site.register(Hero)
 admin.site.register(Villain)
 
 admin.site.register(Category)
+
 
 @admin.register(Origin)
 class OriginAdmin(admin.ModelAdmin):
@@ -19,8 +20,8 @@ class OriginAdmin(admin.ModelAdmin):
         queryset = super().get_queryset(request)
         """Annotate porque estó reduce los queries """
         queryset = queryset.annotate(
-            _hero_count= Count('hero', distinct=True),
-            _villain_count= Count('villain', distinct=True)
+            _hero_count=Count('hero', distinct=True),
+            _villain_count=Count('villain', distinct=True)
         )
         return queryset
 
@@ -29,4 +30,18 @@ class OriginAdmin(admin.ModelAdmin):
 
     def villain_count(self, obj):
         return obj.villain_set.count()
+
     list_display = ('name', 'hero_count', 'villain_count')
+
+    #allow order the Origin admin section by calculated fields that are not present in Origin model
+    hero_count.admin_order_field = '_hero_count'
+    villain_count.admin_order_field = '_villain_count'
+
+@admin.register(Hero)
+class HeroAdmin(admin.ModelAdmin):
+
+    list_display = ("name", "is_immortal", "category", "origin", "is_very_benevolent")
+    list_filter = ("is_immortal", "category", "origin",)
+
+    def is_very_benevolent(self, obj):
+        return obj.benevolence_factor >75
